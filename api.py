b4bccdf478d0530e1717e7047635a2e77489ad7a
Yash <yash1202upadhyay@gmail.com>
2023-08-15 08:00:38 +0530

    Removed Unnecessary Code

--- api.py
+++ api.py
@@ -18,22 +18,3 @@ def api_call(keywords):
     
     return json_data
 
-# CHATGPT API CALLING
-def gpt_api_call(content="Tell me a joke"):
-    url = "https://chatgpt-api6.p.rapidapi.com/standard-gpt"
-
-    payload = { "conversation": [
-            {
-                "role": "user",
-                "content": content
-            }
-        ] }
-    headers = {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": os.getenv('api_key'),
-        "X-RapidAPI-Host": "chatgpt-api6.p.rapidapi.com"
-    }
-
-    response = requests.post(url, json=payload, headers=headers)
-
-    print(response.json())
